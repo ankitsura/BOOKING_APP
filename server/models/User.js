c8e91e3ec0ed44f810545e6413959fc0bd03187d
Ankit <suraankit6@gmail.com>
2023-03-18 15:21:35 +0530

    prev

--- server/models/User.js
+++ server/models/User.js
@@ -1,2 +1,28 @@
+import mongoose from "mongoose";
+
+<<<<<<< HEAD
+const UserSchema = mongoose.Schema({
+=======
+const UserSchema = new mongoose.Schema({
+>>>>>>> f6f3fdb04cdf432de15737349acbe4ee49a789f1
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+}, {timestamps: true});
+
 
 export default mongoose.model('User', UserSchema);
